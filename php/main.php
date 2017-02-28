69d372abad5d8d5d11f1673d7c4cf5a054123074
Toni Nuñez <toninu1989@gmail.com>
2017-03-01 00:04:41 +0100

    All functions and templates declared

---
+++ php/main.php
@@ -0,0 +1,87 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+print_r($_POST);
+
+$host = $_POST['domini'];
+
+
+//Primer switch 
+if(isset($_POST['selectComunicat'])){
+	$primerComunicat = $_POST['selectComunicat'];
+	switch($primerComunicat){
+		case 'com1': templateOne($host);
+		break;
+		case 'com2': primerSwitch();
+		break;
+	}
+}
+
+//Segon switch
+function primerSwitch(){
+	if(isset($_POST['segonComunicat'])){
+		$segonComunicat = $_POST['segonComunicat'];
+		switch($segonComunicat){
+			case 'comSta': segonSwitch();
+			break;
+			case 'comCold': templateTwo($host);
+			break;
+			case 'comDNS': templateThree($host);
+			break;
+		}
+	}
+}
+
+//Tercer switch
+function segonSwitch(){
+    if(isset($_POST['selectSubclass'])){
+        $tercerComunicat = $_POST['selectSubclass'];
+        switch($tercerComunicat){
+            case 'aliasNo': templateFour($host);
+            break;
+            case 'aliasSi': templateFive($host);
+            break;
+        }
+    }
+}
+
+//Templates 
+
+function templateOne($host){
+	ob_start();
+	require('plantilla1.php');
+	$plantilla1 = ob_get_contents();
+	ob_clean();
+	echo $plantilla1;
+}
+function templateTwo($host){
+	ob_start();
+	require('plantilla2.php');
+	$plantilla2 = ob_get_contents();
+	ob_clean();
+	echo $plantilla2;
+}
+function templateThree($host){
+	ob_start();
+	require('plantilla3.php');
+	$plantilla3 = ob_get_contents();
+	ob_clean();
+	echo $plantilla3;
+}
+function templateFour($host){
+	ob_start();
+	require('plantilla4.php');
+	$plantilla4 = ob_get_contents();
+	ob_clean();
+	echo $plantilla4;
+}
+function templateFive($host){
+	ob_start();
+	require('plantilla5.php');
+	$plantilla5 = ob_get_contents();
+	ob_clean();
+	echo $plantilla5;
+}
+?>
---
+++ php/plantilla1.php
@@ -0,0 +1,18 @@
+
+<html>
+Benvolgut client,</br>
+Ens posem en contacte amb vosaltres en relació als serveis d'allotjament (hosting) que actualment teniu contractats amb ILIMIT (<a href="<?= $host ?>"> <?=  $host ?></a> ).</br>
+ Us informem que el període de suport de la versió de PHP utilitzada a la plataforma d'allotjament on esteu ubicats ha acabat, el que suposa que des de fa temps no es fan actualitzacions de seguretat i això suposa un risc així com una possible causa d'incidències. </br>
+</br>
+A continuació enumerem les versions i la data en que va acabar el període oficial de suport de PHP:</br>
+</br>
+* PHP 4.x (període de suport caducat fa més de 6 anys)</br>
+* PHP 5.2 (període de suport caducat fa més de 4 anys)</br>
+* PHP 5.3 (període de suport caducat fa més de d'1 any)</br>
+</br>
+Amb aquest motiu, properament us contactarem per coordinar una migració del vostre allotjament de hosting cap a una nova plataforma amb més possibilitats de millora i actualitzacions de seguretat.</br>
+</br>
+Si teniu cap dubte estem a la vostra disposició, podeu contactar amb nosaltres per telèfon o bé per correu a suport@ilimit.com.</br>
+</br>
+Salutacions cordials.</br>
+</html>
